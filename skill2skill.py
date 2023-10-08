464b11a42f57deff29e18313f6d2e608d96ccbb5
neeeal <matira.nealbartonjames@gmail.com>
2023-10-08 15:09:36 +0800

    allowed user input through console

--- skill2skill.py
+++ skill2skill.py
@@ -1,5 +1,6 @@
 from gensim.models import KeyedVectors
 
+
 def load_wv(path="wordvectors\word2vec.wordvectors"):
     ## Load word vectors
     wv = KeyedVectors.load(path, mmap='r')
@@ -15,10 +16,25 @@ def get_most_similary(input_array, wv, topn=10):
     output = {'input':input_array,'similars':{sim[0]:sim[1] for sim in sims}}
     return output
     
-# wv = load_wv()
-# input_array = ['database']
-# processed_input = preprocess_input(input_array, wv)
-# sims = get_most_similary(processed_input, wv)
+if __name__ == '__main__': 
+    import nltk
+    from nltk.corpus import stopwords
+    nltk.download('stopwords')
+    import re
 
-# print(sims)
+    def input_to_array(input, stop_words = stopwords.words('english')):
+        ## Retrieving job descriptions
+        ## Applying NLP: lowercase, remove non-alphanum, remove stopwords
+        temp = re.sub(r"[^ a-zA-Z]+",' ', input).lower().split(' ')
+        temp = [word for word in temp if word not in stop_words if len(word) >= 1]
+        return temp
+
+    wv = load_wv()
+
+    while True:
+        input_array = input_to_array(input())
+        if input_array[0] == 'q': break
+        processed_input = preprocess_input(input_array, wv)
+        sims = get_most_similary(processed_input, wv)
+        print(sims)
 
